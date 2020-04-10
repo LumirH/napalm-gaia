9ab057801f9a8d25fe1d4ef7bb6250006f275ea0
remingu <remingu@techturn.de>
2020-04-10 13:42:54 +0200

    merged vsx context switch handling

--- napalm_gaiaos/gaiaos.py
+++ napalm_gaiaos/gaiaos.py
@@ -1,3 +1,19 @@
+#
+# Copyright 2020 Daniel Schlifka, Pavel Smejkal
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import logging
 import statistics
 import time
@@ -1041,6 +1057,9 @@ class GaiaOSDriver(NetworkDriver):
                 raise ValidationException('VSX not enabled')
         except (socket.error, EOFError) as e:
             raise ConnectionClosedException(str(e))
+
+    def _get_cpenv(self):
+        pass
 
     ##########################################################################################
     # """                               the tbd section                                  """ #
