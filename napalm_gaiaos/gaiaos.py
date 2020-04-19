677ba34e5146eb2a2cffd60c3ae97fc837c2e44d
remingu <remingu@techturn.de>
2020-04-19 08:37:35 +0200

    added _check_for_dclish()

--- napalm_gaiaos/gaiaos.py
+++ napalm_gaiaos/gaiaos.py
@@ -1068,10 +1068,19 @@ class GaiaOSDriver(NetworkDriver):
 
     def _get_cpenv(self):
         self._get_default_shell()
+        self.vsx_state = self._check_vsx_state()
+        dclish_re = re.compile(r'[Dd]epricated.*sk144112.*')
+        clish_re = re.compile(r'[Uu]sage.*')
+
+
+    def _get_default_shell(self):
+        if self._check_expert_mode() is False:
+            self.shell_default_clish = True
+
+    def _check_for_dclish(self):
         dclish_re = re.compile(r'[Dd]epricated.*sk144112.*')
         clish_re = re.compile(r'[Uu]sage.*')
         if self.shell_default_clish is True:
-            self.vsx_state = self._check_vsx_state()
             tmpstr = self.device.send_command('cplic')
             if re.match(dclish_re, tmpstr) is not None:
                 self.dclish = True
@@ -1082,10 +1091,6 @@ class GaiaOSDriver(NetworkDriver):
                 raise RuntimeError(msg)
         else:
             pass
-
-    def _get_default_shell(self):
-        if self._check_expert_mode() is False:
-            self.shell_default_clish = True
 
     ##########################################################################################
     # """                               the tbd section                                  """ #
